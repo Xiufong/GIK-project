2007909d2a03576f874aa3ca103cfdb20fab12c0
xiufong <xiufong@xius-mbp.uai.cl>
2015-05-06 14:42:27 -0400

    se agrega install de la base de datos

--- db/upgrade.php
+++ db/upgrade.php
@@ -24,8 +24,8 @@
  * it cannot do itself, it will tell you what you need to do.  The commands in
  * here will all be database-neutral, using the functions defined in DLL libraries.
  *
- * @package    mod_newmodule
- * @copyright  2015 Your Name
+ * @package    mod_throwquestions
+ * @copyright  2015 Xiu-Fong Lin <xlin@alumnos.uai.cl>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -37,7 +37,7 @@ defined('MOODLE_INTERNAL') || die();
  * @param int $oldversion
  * @return bool
  */
-function xmldb_newmodule_upgrade($oldversion) {
+function xmldb_throwquestions_upgrade($oldversion) {
     global $DB;
 
     $dbman = $DB->get_manager(); // Loads ddl manager and xmldb classes.
@@ -68,167 +68,87 @@ function xmldb_newmodule_upgrade($oldversion) {
      *
      * First example, some fields were added to install.xml on 2007/04/01
      */
-    if ($oldversion < 2015050600) {
-    
-    	// Define table score to be created.
-    	$table = new xmldb_table('score');
-    
-    	// Adding fields to table score.
-    	$table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-    	$table->add_field('battle_id', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('winner', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('loser', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-    
-    	// Adding keys to table score.
-    	$table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-    
-    	// Conditionally launch create table for score.
-    	if (!$dbman->table_exists($table)) {
-    		$dbman->create_table($table);
-    	}
-    
-    	// Throwquestions savepoint reached.
-    	upgrade_mod_savepoint(true, 2015050600, 'throwquestions');
+    if ($oldversion < 2015050602) {
+
+        // Define table score to be created.
+        $table = new xmldb_table('score');
+
+        // Adding fields to table score.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('battle_id', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('winner', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('loser', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table score.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for score.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Throwquestions savepoint reached.
+        upgrade_mod_savepoint(true, 2015050602, 'throwquestions');
     }
     
-    if ($oldversion < 2015050600) {
     
-    	// Define table battle to be created.
-    	$table = new xmldb_table('battle');
-    
-    	// Adding fields to table battle.
-    	$table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-    	$table->add_field('sender_id', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('receiver_id', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('question', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('correct', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('incorrect_1', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('incorrect_2', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('incorrect_3', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('status', XMLDB_TYPE_BINARY, null, null, XMLDB_NOTNULL, null, null);
-    
-    	// Adding keys to table battle.
-    	$table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-    
-    	// Conditionally launch create table for battle.
-    	if (!$dbman->table_exists($table)) {
-    		$dbman->create_table($table);
-    	}
-    
-    	// Throwquestions savepoint reached.
-    	upgrade_mod_savepoint(true, 2015050600, 'throwquestions');
+    if ($oldversion < 2015050602) {
+
+        // Define table battle to be created.
+        $table = new xmldb_table('battle');
+
+        // Adding fields to table battle.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('sender_id', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('receiver_id', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('question', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('correct', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('incorrect_1', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('incorrect_2', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('incorrect_3', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table battle.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for battle.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Throwquestions savepoint reached.
+        upgrade_mod_savepoint(true, 2015050602, 'throwquestions');
     }
     
-    if ($oldversion < 2015050600) {
     
-    	// Define table throwquestions to be created.
-    	$table = new xmldb_table('throwquestions');
-    
-    	// Adding fields to table throwquestions.
-    	$table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-    	$table->add_field('course', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('intro', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('introformat', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, '0');
-    	$table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
-    	$table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
-    	$table->add_field('grade', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '100');
-    
-    	// Adding keys to table throwquestions.
-    	$table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-    
-    	// Adding indexes to table throwquestions.
-    	$table->add_index('course', XMLDB_INDEX_NOTUNIQUE, array('course'));
-    
-    	// Conditionally launch create table for throwquestions.
-    	if (!$dbman->table_exists($table)) {
-    		$dbman->create_table($table);
-    	}
-    
-    	// Throwquestions savepoint reached.
-    	upgrade_mod_savepoint(true, 2015050600, 'throwquestions');
+    if ($oldversion < 2015050601) {
+
+        // Define table throwquestions to be created.
+        $table = new xmldb_table('throwquestions');
+
+        // Adding fields to table throwquestions.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+
+        // Adding keys to table throwquestions.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for throwquestions.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Throwquestions savepoint reached.
+        upgrade_mod_savepoint(true, 2015050601, 'throwquestions');
     }
     
-    if ($oldversion < 2007040100) {
-
-        // Define field course to be added to newmodule.
-        $table = new xmldb_table('newmodule');
-        $field = new xmldb_field('course', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'id');
-
-        // Add field course.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define field intro to be added to newmodule.
-        $table = new xmldb_table('newmodule');
-        $field = new xmldb_field('intro', XMLDB_TYPE_TEXT, 'medium', null, null, null, null, 'name');
-
-        // Add field intro.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define field introformat to be added to newmodule.
-        $table = new xmldb_table('newmodule');
-        $field = new xmldb_field('introformat', XMLDB_TYPE_INTEGER, '4', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0',
-            'intro');
-
-        // Add field introformat.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Once we reach this point, we can store the new version and consider the module
-        // ... upgraded to the version 2007040100 so the next time this block is skipped.
-        upgrade_mod_savepoint(true, 2007040100, 'newmodule');
-    }
-
-    // Second example, some hours later, the same day 2007/04/01
-    // ... two more fields and one index were added to install.xml (note the micro increment
-    // ... "01" in the last two digits of the version).
-    if ($oldversion < 2007040101) {
-
-        // Define field timecreated to be added to newmodule.
-        $table = new xmldb_table('newmodule');
-        $field = new xmldb_field('timecreated', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0',
-            'introformat');
-
-        // Add field timecreated.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define field timemodified to be added to newmodule.
-        $table = new xmldb_table('newmodule');
-        $field = new xmldb_field('timemodified', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0',
-            'timecreated');
-
-        // Add field timemodified.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define index course (not unique) to be added to newmodule.
-        $table = new xmldb_table('newmodule');
-        $index = new xmldb_index('courseindex', XMLDB_INDEX_NOTUNIQUE, array('course'));
-
-        // Add index to course field.
-        if (!$dbman->index_exists($table, $index)) {
-            $dbman->add_index($table, $index);
-        }
-
-        // Another save point reached.
-        upgrade_mod_savepoint(true, 2007040101, 'newmodule');
-    }
+    
 
     // Third example, the next day, 2007/04/02 (with the trailing 00),
     // some actions were performed to install.php related with the module.
-    if ($oldversion < 2007040200) {
+    if ($oldversion < 2015050600) {
 
         // Insert code here to perform some actions (same as in install.php).
 
-        upgrade_mod_savepoint(true, 2007040200, 'newmodule');
+        upgrade_mod_savepoint(true, 2015050600, 'newmodule');
     }
 
     /*
